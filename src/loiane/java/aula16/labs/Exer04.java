ba039ddd327ab9d9fdd7e58ef0d2e74b9bc3f36b
pigjao <joaovictorbagomes@gmail.com>
2023-04-10 23:03:04 -0300

    Exercicio04

---
+++ src/loiane/java/aula16/labs/Exer04.java
@@ -0,0 +1,20 @@
+package loiane.java.aula16.labs;
+//Não fiz o commit nas últimas alterações e perdi as atividades
+public class Exer04 {
+    public static void main(String[] args) {
+        int populacaoBrasil = 80000;
+        int populacaoArgentina = 200000;
+        int ano = 0;
+
+        while (populacaoBrasil < populacaoArgentina){
+
+            populacaoBrasil += (populacaoBrasil / 100) * 3;
+            populacaoArgentina += (populacaoArgentina / 100) * 1.5;
+            ano++;
+        }
+        System.out.println("População do Brasil: " + populacaoBrasil);
+        System.out.println("População do EUA: " + populacaoArgentina);
+        System.out.println("Será necessário " + ano + " anos para que a população do Brasil seja maior que a população dos EUA");
+
+    }
+}
